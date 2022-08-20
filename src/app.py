d7571f8caffbb03586ef62f7e12a6089f19af34c
Guillermo Fernando Sanchez Albarracin <guillermo.sanchez@utec.edu.pe>
2022-08-19 19:53:25 -0500

    avance login/register

--- src/app.py
+++ src/app.py
@@ -26,3 +26,28 @@ def deleteProduct():
     prod = Product.get(Product.id = id)
     prod.delete_instance()
     return Response(200)
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        login = User.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            #redirect page
+    #render login
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+        phon = reques.form['phon']
+        addressaux = request.form['addressaux']
+        rol = request.form['rol']
+        register = user(username = uname, email = mail, password = passw, phone = phon, address = addressaux, rol_id = rol)
+        db.session.add(register)
+        db.session.commit()
+        #redirect login
+    #render register
